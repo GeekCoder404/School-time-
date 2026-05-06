b61d75717fbf08ce0d02a807304904855f076dcf
GeekCoder404 <gauthierc2+54@cssdd.ca>
2026-05-06 11:30:09 -0400

    dsadas

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d', { alpha: true });
 const pipBtn = document.getElementById('pipBtn');
 const video = document.getElementById('video');
 
@@ -7,7 +7,8 @@ const video = document.getElementById('video');
 const pNames = ['P1', 'P2', 'PIC', 'P3', 'P4'];
 const sTimes = ['08h10', '09h35', '10h55', '12h35', '14h00'];
 const eTimes = ['09h20', '10h45', '11h15', '13h45', '15h10'];
-let clr = '#36d3ff';
+let usrclr = localStorage.getItem('clr') || '#36d3ff';
+let clr = '';
 
 // ---------------- TIME HELPERS ----------------
 function parseTime(str) {
@@ -36,7 +37,7 @@ function getText() {
 
   let target = null;
   let index = 0;
-  clr = '#36d3ff';
+  clr = usrclr;
   // during class
   for (let i = 0; i < starts.length; i++) {
     if (nowAdj >= starts[i] && nowAdj < ends[i]) {
@@ -69,17 +70,17 @@ function getText() {
   const h = Math.floor(totalSec / 3600).pad();
   const m = Math.floor((totalSec % 3600) / 60).pad();
   const s = (totalSec % 60).pad();
-  if (h == 0 && m <= 5) clr = '#ffff00';
-  if (h == 0 && m <= 1 && s <= 30) clr = '#ff0000';
+  //if (h == 0 && m <= 5) clr = '#ffff00';
+  //if (h == 0 && m <= 1 && s <= 30) clr = '#ff0000';
   return `${pNames[index]} ${h}h ${m}m ${s}s`;
 }
 
 // ---------------- CANVAS RENDER ----------------
 function draw() {
-  const text = getText();
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  ctx.fillStyle = '#1f1f1f';
+  const text = getText();
+  ctx.fillStyle = '#;fds
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   ctx.fillStyle = clr;
@@ -117,4 +118,20 @@ pipBtn.addEventListener('click', async () => {
   } catch (err) {
     console.error('PiP error:', err);
   }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  const clr = document.querySelector('#clr');
+  document.querySelectorAll('.clring').forEach((el) => {
+    el.target.style.color = usrclr;
+  });
+  clr.value = usrclr;
+});
+
+document.querySelector('#clr').addEventListener('change', (e) => {
+  usrclr = e.target.value;
+  localStorage.setItem('clr', usrclr);
+  document.querySelectorAll('.clring').forEach((el) => {
+    el.target.style.color = usrclr;
+  });
 });
